1ef0c24e972ee756ed1e071f86dbd55ce95cfe01
Jacob Bashista <tgb.bashista@gmail.com>
2021-05-25 18:41:50 -0400

    Setup

---
+++ src/main/java/gg/tgb/twitchplaysmc/TwitchPlaysMC.java
@@ -0,0 +1,11 @@
+package gg.tgb.twitchplaysmc;
+
+import net.fabricmc.api.ModInitializer;
+
+public class TwitchPlaysMC implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        System.out.println("TwitchPlaysMC Initialized");
+    }
+}
